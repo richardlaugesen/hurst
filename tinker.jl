61985413cec09f0558ec85f5baa5f526e9a82d24
Richard <richard@tinyrock.com>
2018-11-12 22:26:19 +1100

    Simulation with simple one store, two param rainfall-runoff model

--- tinker.jl
+++ tinker.jl
@@ -59,3 +59,57 @@ end
 log_q = boxcox(data.obs_runoff, 0)
 trans_q = boxcox(data.obs_runoff, 0.2)
 histogram([log_q, trans_q])
+
+# a simple one storage, two parameter rainfall-runoff model
+function one_store_model(rain, pet, storage, params)
+    capacity = params["capacity"]
+    loss = params["loss"]
+
+    # effective rainfall
+    storage += rain - pet
+
+    # general losses
+    storage -= loss
+    storage = (storage > 0) ? storage : 0
+
+    # runoff generation
+    if storage > capacity
+        runoff = storage - capacity
+        storage = capacity
+    else
+        runoff = 0
+    end
+
+    return storage, runoff
+end
+
+# try the model out
+params = Dict("capacity" => 30, "loss" => 2)
+one_store_model(50, 5, 4, params)
+
+# create new dataframe for model input and output and initalise storage to 0
+# TODO: fill with missing instead of 0.0 but need to define type to be Float32 somehow
+len = nrow(data)
+model_output = DataFrame(storage = fill(0.0, len), runoff_sim = fill(0.0, len))
+model_data = hcat(data, model_output)
+
+# run model over series
+for i in 1:len
+    rain = model_data[i, :obs_rain]
+    pet = model_data[i, :obs_pet]
+    curr_storage = model_data[i, :storage]
+
+    new_storage, runoff = one_store_model(rain, pet, curr_storage, params)
+
+    model_data[i, :runoff_sim] = runoff
+
+    if i < len
+        model_data[i + 1, :storage] = new_storage
+    end
+end
+
+# see how the results look
+d = model_data[s:e, [:obs_runoff, :runoff_sim, :obs_runoff_sim_0, :storage]]
+println(d)
+plot([d.obs_runoff, d.runoff_sim, d.obs_runoff_sim_0])
+plot(d.storage)
