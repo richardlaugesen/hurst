4cba06fc316a861d21f388e4488c1706d45fc618
Richard <richard@tinyrock.com>
2018-11-21 09:55:00 +1100

    Added inverse boxcox transformation

--- transformations.jl
+++ transformations.jl
@@ -3,6 +3,14 @@ function boxcox(y, λ)
     if λ == 0
         log.(y)
     else
-        (y.^λ .- 1) ./ λ
+        (y.^λ .- 1) / λ
+    end
+end
+
+function boxcox_inverse(z, λ)
+    if λ == 0
+        exp.(z)
+    else
+        (λ*z .+ 1).^(1/λ)
     end
 end
