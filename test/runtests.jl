30fde5f59659c37759bf74f0fb75dba64e6dafa3
Richard <richard@tinyrock.com>
2018-11-21 23:21:37 +1100

    Breakout component tests from main test runner, and load HydroJulia

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,8 @@
 using Test
-using HydroJulia
 
-@testset "Box-cox transformation" begin
-    y = rand(1000)
+include("../src/HydroJulia.jl")
+using .HydroJulia
 
-    @testset "λ = 0" begin
-        λ = 0
-        @test_throws DomainError boxcox(-1, λ)
-        @test boxcox(0, λ) == -Inf
-        @test boxcox(2, λ) == log(2)
-        @test boxcox([1, 1, 1, 1], λ) == [0, 0, 0, 0]
-        @test boxcox_inverse(boxcox(y, λ), λ) ≈ y
-    end
-
-    @testset "λ = 0.2" begin
-        λ = 0.2
-        @test_throws DomainError boxcox(-1, λ)
-        @test boxcox(0, λ) == -1 / λ
-        @test boxcox(2, λ) == (2^λ - 1) / λ
-        @test boxcox([1, 1, 1, 1], λ) == [0, 0, 0, 0]
-        @test boxcox_inverse(boxcox(y, λ), λ) ≈ y
-    end
-
-    @testset "λ=$λ (range testset)" for λ in -3:0.1:3
-        @test boxcox_inverse(boxcox(y, λ), λ) ≈ y
-    end
+@testset "Verification" begin
+    include("verification.jl")
 end
