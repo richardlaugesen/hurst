56796a44ffc914a4af97efbab536d35e27e0f92f
Richard <richard@tinyrock.com>
2018-12-11 23:37:23 +1100

    Tidy up

--- src/simulate.jl
+++ src/simulate.jl
@@ -8,7 +8,6 @@ function simulate(timestep, data, pars, init_state)
     for i in 1:len
         sim[i], new_state = timestep(rain[i], pet[i], curr_state, pars)
         curr_state = new_state
-        #println(runoff, " ", curr_state["production_store"], " ", curr_state["routing_store"])
     end
 
     data["runoff_sim"] = sim
