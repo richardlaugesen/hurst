ab3733b77968c93a74d1723e5ab6dd6b1aa6a14f
Richard <richard@tinyrock.com>
2018-12-17 21:34:28 +1100

    Replaced Dict string keys with symbols

--- src/verification.jl
+++ src/verification.jl
@@ -10,36 +10,41 @@ function coeff_det(y, f)
     return 1 - sum(skipmissing(y - f).^2) / sum(skipmissing(y .- mean(skipmissing(y))).^2)
 end
 
+# mean square error
 function mse(o, s)
     sum(skipmissing(o - s).^2) / (length_no_missing(o) - 1)
 end
 
+# mean absolute error
 function mae(o, s)
     sum(abs.(skipmissing(o - s))) / (length_no_missing(o) - 1)
 end
 
+# root mean square error
 function rmse(o, s)
     sqrt(mse(o, s))
 end
 
+# persistence index
 function persistence(o, s)
     shifted_o = lshift(o)
     return 1 - sum(skipmissing(s - o).^2) / sum(skipmissing(shifted_o - o).^2)
 end
 
-function kge(o, s, components)  
+# kling-gupta efficiency
+function kge(o, s, components)
     o, s = dropna(o, s)
     r = cov(o, s, corrected=true) / (std(s, corrected=true) * std(o, corrected=true))  # covariance
     a = std(s) / std(o, corrected=true)  # relative variability
     b = mean(s) / mean(o)  # mean bias
     k = 1 - sqrt((r - 1)^2 + (a - 1)^2 + (b - 1)^2)
-    
+
     if components
         return Dict(
-            "covariance" => r,
-            "relative_variability" => a,
-            "mean_bias" => b,
-            "kge" => k
+            :covariance => r,
+            :relative_variability => a,
+            :mean_bias => b,
+            :kge => k
         )
     else
         return k
@@ -47,4 +52,3 @@ function kge(o, s, components)
 end
 
 kge(o, s) = kge(o, s, false)
-
