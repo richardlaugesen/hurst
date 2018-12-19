97f25405d9342b41f62b30d2374f04c3b3176208
Richard <richard@tinyrock.com>
2018-12-19 12:05:50 +1100

    Package level testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,16 @@ using Test
 include("../src/Hydro.jl")
 using .Hydro
 
-@testset "Rainfall funoff models" begin
-    include("gr4j.jl")
-end
+@testset "Hydro.jl" begin
+    @testset "Rainfall funoff models" begin
+        include("gr4j.jl")
+    end
 
-@testset "Transforms" begin
-    include("transforms.jl")
-end
+    @testset "Transforms" begin
+        include("transforms.jl")
+    end
 
-@testset "Verification" begin
-    include("verification.jl")
+    @testset "Verification" begin
+        include("verification.jl")
+    end
 end
