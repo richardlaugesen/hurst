c41f33f550ec24b88129610c659c709862453fd7
Richard <richard@tinyrock.com>
2019-01-29 12:44:25 +1100

    Changed licence to LGPL

--- src/visualisation.jl
+++ src/visualisation.jl
@@ -1,18 +1,35 @@
+# Copyright 2019 Richard Laugesen
+#
+# This file is part of Hydro.jl
+#
+# Hydro.jl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hydro.jl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Hydro.jl.  If not, see <https://www.gnu.org/licenses/>.
+
 using Plots
 
 function hydrograph(rain, runoffs, runoff_labels)
     p1 = plot(
-            rain,         
+            rain,
             seriestype=:bar,
-            ylabel="Rain (mm)", 
-            legend=false, 
+            ylabel="Rain (mm)",
+            legend=false,
             grid=true,
             yflip=true,
             xaxis=false)
 
     p2 = plot(
-            runoffs, 
-            xlabel="Timestep", ylabel="Runoff (mm)", 
+            runoffs,
+            xlabel="Timestep", ylabel="Runoff (mm)",
             label=runoff_labels,
             grid=true,
             lw=1.5)
