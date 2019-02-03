e048f3087bcbedce22e9f8cb47e6f7142fbe7672
Richard <richard@tinyrock.com>
2019-02-03 17:14:22 +1100

    Remove return on single line function

--- src/verification.jl
+++ src/verification.jl
@@ -24,7 +24,7 @@ end
 
 # coefficient of determination
 function coeff_det(y, f)
-    return 1 - sum(skipmissing(y - f).^2) / sum(skipmissing(y .- mean(skipmissing(y))).^2)
+    1 - sum(skipmissing(y - f).^2) / sum(skipmissing(y .- mean(skipmissing(y))).^2)
 end
 
 # mean square error
