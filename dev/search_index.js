9a03f5fdcbd4a41a4579a114a1cf2e76ee7ea35e
autodocs <autodocs>
2019-07-18 22:56:12 +0000

    build based on fac7de4

--- dev/search_index.js
+++ dev/search_index.js
@@ -457,46 +457,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#Hurst.Verification.confusion-NTuple{4,Any}",
-    "page": "Hurst Documentation",
-    "title": "Hurst.Verification.confusion",
-    "category": "method",
-    "text": "confusion(hits, misses, false_alarms, quiets)\n\nReturns a Dict containing fields of a confusion matrix with the various common names used for each combination of the 2x2 grid.\n\nSee also: confusion_scaled(hits, misses, false_alarms, quiets)\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Hurst.Verification.confusion_scaled-NTuple{4,Any}",
-    "page": "Hurst Documentation",
-    "title": "Hurst.Verification.confusion_scaled",
-    "category": "method",
-    "text": "confusion_scaled(hits, misses, false_alarms, quiets)\n\nReturns a Dict containing fields of a confusion matrix with the various common names used for each combination of the 2x2 grid scaled to be relative to the number of events and non-events (hits + misses + false_alarms + quiets).\n\nSee also: confusion(hits, misses, false_alarms, quiets)\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Hurst.Verification.cost_loss-Tuple{Any,Any,Any}",
-    "page": "Hurst Documentation",
-    "title": "Hurst.Verification.cost_loss",
-    "category": "method",
-    "text": "cost_loss(costs, losses, scaled_conf, method=\"verkade\")\n\nReturns the relative economic value of a forecast system using a the cost-loss model using the cost-loss ratio (costs, losses) and confusion matrix scaled by the total events and non-events scaled_conf.\n\nTwo methods are implemented (Verkade 2011 and Roulin 2007) which may be selected with the method argument (default is Verkade).\n\nVerkade, J. S., and M. G. F. Werner. “Estimating the Benefits of Single Value and Probability Forecasting for Flood Warning.” Hydrology and Earth System Sciences 15, no. 12 (December 20, 2011): 3751–65. https://doi.org/10.5194/hess-15-3751-2011.\n\nRoulin, E. “Skill and Relative Economic Value of Medium-Range Hydrological Ensemble Predictions.” Hydrology and Earth System Sciences 11, no. 2 (2007): 725–37. https://doi.org/10.5194/hess-11-725-2007.\n\nSee also: confusion_scaled(hits, misses, false_alarms, quiets), cost_loss_roulin(costs, losses, scaled_conf), cost_loss_verkade(costs, losses, scaled_conf)\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Hurst.Verification.cost_loss_roulin-Tuple{Any,Any,Any}",
-    "page": "Hurst Documentation",
-    "title": "Hurst.Verification.cost_loss_roulin",
-    "category": "method",
-    "text": "cost_loss_roulin(costs, losses, scaled_conf)\n\nReturns the relative economic value of a forecast system using a the cost-loss model using the cost-loss ratio (costs, losses) and confusion matrix scaled by the total events and non-events scaled_conf.\n\nRoulin, E. “Skill and Relative Economic Value of Medium-Range Hydrological Ensemble Predictions.” Hydrology and Earth System Sciences 11, no. 2 (2007): 725–37. https://doi.org/10.5194/hess-11-725-2007.\n\nSee also: confusion_scaled(hits, misses, false_alarms, quiets), cost_loss(costs, losses, scaled_conf, method), cost_loss_verkade(costs, losses, scaled_conf)\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Hurst.Verification.cost_loss_verkade-Tuple{Any,Any,Any}",
-    "page": "Hurst Documentation",
-    "title": "Hurst.Verification.cost_loss_verkade",
-    "category": "method",
-    "text": "cost_loss_verkade(costs, losses, scaled_conf)\n\nReturns the relative economic value of a forecast system using a the cost-loss model using the cost-loss ratio (costs, losses) and confusion matrix scaled by the total events and non-events scaled_conf.\n\nVerkade, J. S., and M. G. F. Werner. “Estimating the Benefits of Single Value and Probability Forecasting for Flood Warning.” Hydrology and Earth System Sciences 15, no. 12 (December 20, 2011): 3751–65. https://doi.org/10.5194/hess-15-3751-2011.\n\nSee also: confusion_scaled(hits, misses, false_alarms, quiets), cost_loss(costs, losses, scaled_conf, method), cost_loss_roulin(costs, losses, scaled_conf)\n\n\n\n\n\n"
-},
-
-{
     "location": "#Hurst.Verification.kge-Tuple{Any,Any,Any}",
     "page": "Hurst Documentation",
     "title": "Hurst.Verification.kge",
